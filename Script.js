f7c882e45d646033a39d3a63ff882d95e49ea33b
Self-pK <122510739+Self-pK@users.noreply.github.com>
2023-01-12 15:59:24 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,16 @@
+{
+  let myConnector = tableau.makeConnector();
+
+myConnector.getSchema = function (schemaCallback) {
+  // Add code here
+};
+
+myConnector.getData = function (table, doneCallback) {
+  // Add code here
+};
+
+tableau.registerConnector(myConnector);
+
+function submit() {
+  // Add code here
+}
